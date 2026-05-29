656196e9616f089ce9f88f95aab5804a911d09f5
DanielAsDano <daniel.cardenas1767@gmail.com>
2026-05-29 15:22:08 -0600

    añadir funcion al agregarProducto

--- src/app/agregar-producto/agregar-producto.component.ts
+++ src/app/agregar-producto/agregar-producto.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Producto } from '../producto';
+import { ProductoService } from '../producto.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-agregar-producto',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './agregar-producto.component.html',
 })
 export class AgregarProductoComponent {
+  producto: Producto = new Producto();
 
+  private productoServicio = inject(ProductoService);
+  private enrutador = inject(Router)
+
+  onSubmit(){
+    this.guardarProducto();
+  }
+
+  guardarProducto(){
+    this.productoServicio.agregarProducto(this.producto).subscribe({
+      next: (datos) =>{
+        this.irListaProductos();
+      },
+      error: (error: any) => (console.log(error))
+    })
+  }
+
+  irListaProductos(){
+    this.enrutador.navigate(['/productos'])
+  }
 }
