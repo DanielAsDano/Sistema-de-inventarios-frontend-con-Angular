254abec77ce2bad918f28cedefe26955150d27f1
DanielAsDano <daniel.cardenas1767@gmail.com>
2026-05-31 00:30:27 -0600

    añadir boton de eliminar mas funcionalidad en el servicio

--- src/app/producto.service.ts
+++ src/app/producto.service.ts
@@ -27,4 +27,8 @@ export class ProductoService {
   editarProducto(id: number, producto: Producto){
     return this.clienteHttp.put(`${this.urlBase}/${id}`, producto);
   }
+
+  eliminarProducto(id: number): Observable<Object>{
+    return this.clienteHttp.delete(`${this.urlBase}/${id}`);
+  }
 }
